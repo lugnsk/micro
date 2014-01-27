4e660f847e02645730134412d889b5091d24fb6c
Oleg Lunegov <zcasperx@gmail.com>
2014-01-27 04:44:04 -0800

    Add phpdoc code

--- micro/base/MAutoload.php
+++ micro/base/MAutoload.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * MAutoload class file.
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/antivir88/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license http://opensource.org/licenses/MIT
+ * @package micro
+ * @version 1.0
+ * @since 1.0
+ */
 class MAutoload
 {
 	/**
