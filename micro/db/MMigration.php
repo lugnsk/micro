3e984c7008d878f493a442f183b1263f3ce9ad76
Oleg Lunegov <zcasperx@gmail.com>
2014-04-02 18:54:33 +0000

    Fix phpdoc

--- micro/db/MMigration.php
+++ micro/db/MMigration.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * MMigration class file.
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/antivir88/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license http://opensource.org/licenses/MIT
+ * @package micro
+ * @subpackage db
+ * @version 1.0
+ * @since 1.0
+ */
 class MMigration {
-
+	/** @var MDbConnection $db */
 	private $db = false;
+
 
 	/**
 	 * Constructor for model
