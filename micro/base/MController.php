7119fc3c4f0590cbd822a4767c4fd95adb5ffbe9
Oleg Lunegov <zcasperx@gmail.com>
2014-04-02 18:57:12 +0000

    Fix phpdoc

--- micro/base/MController.php
+++ micro/base/MController.php
@@ -24,6 +24,7 @@ class MController
 	/** @var array $widgetStack */
 	private $widgetStack = array();
 
+
 	/**
 	 * Contructor for this class
 	 * @return void
@@ -110,7 +111,6 @@ class MController
 
 		return $output;
 	}
-
 	/**
 	 * Render file by path
 	 *
