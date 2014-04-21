21b7e123ca1e64b2657801bb08e0a7d113ff3b1d
Oleg Lunegov <zcasperx@gmail.com>
2014-04-21 19:02:17 +0000

    delete from app

--- app/views/stat/index.php
+++
@@ -1,4 +0,0 @@
-<h2>Статистика</h2>
-<p>Список дублей email: <?php foreach ($emails AS $user) { echo '<br />' . $user->email; } ?></p>
-<p>Список пользователей без заказов: <?php foreach ($norders AS $user) { echo '<br />' . $user->login; } ?></p>
-<p>Список пользователей заказавших более 2 раз: <?php foreach ($moreorders AS $user) { echo '<br />' . $user->login; } ?></p>
