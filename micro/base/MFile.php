dec484998e3d43220bd1c8e8ff957fa90574103c
antivir88 <Poklo09o>
2014-04-25 00:42:34 +0700

    clean code

--- micro/base/MFile.php
+++ micro/base/MFile.php
@@ -1,7 +1,26 @@
 <?php
 
+/**
+ * MFile io class
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/antivir88/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license http://opensource.org/licenses/MIT
+ * @package micro
+ * @version 1.0
+ * @since 1.0
+ */
 class MFile
 {
+	/**
+	 * Recursive copy files
+	 *
+	 * @access public
+	 * @param string $src
+	 * @param string $dst
+	 * @return void
+	 */
 	public static function recurseCopy($src,$dst) {
 		$dir = opendir($src);
 		@mkdir($dst, 0777);
@@ -9,7 +28,7 @@ class MFile
 		while(false !== ( $file = readdir($dir)) ) {
 			if (( $file != '.' ) && ( $file != '..' )) {
 				if ( is_dir($src . '/' . $file) ) {
-					self::recurse_copy($src . '/' . $file,$dst . '/' . $file);
+					self::recurseCopy($src . '/' . $file,$dst . '/' . $file);
 				}
 				else {
 					copy($src . '/' . $file,$dst . '/' . $file);
@@ -19,7 +38,14 @@ class MFile
 		}
 		closedir($dir);
 	}
-
+	/**
+	 * Recursive copy files if edited
+	 *
+	 * @access public
+	 * @param string $src
+	 * @param string $dst
+	 * @return void
+	 */
 	public static function recurseCopyIfEdited($src,$dst) {
 		$dir = opendir($src);
 		@mkdir($dst, 0777);
@@ -27,7 +53,7 @@ class MFile
 		while(false !== ( $file = readdir($dir)) ) {
 			if (( $file != '.' ) && ( $file != '..' )) {
 				if ( is_dir($src . '/' . $file) ) {
-					self::recurse_copy($src . '/' . $file,$dst . '/' . $file);
+					self::recurseCopyIfEdited($src . '/' . $file,$dst . '/' . $file);
 				}
 				else {
 					if (filemtime($src . '/' . $file) != filemtime($dst . '/' . $file)) {
