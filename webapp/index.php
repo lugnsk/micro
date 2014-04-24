db643b5a9d27957d0cdae10baa2406bdd9913d66
antivir88 <Poklo09o>
2014-04-24 23:15:45 +0700

    clean code and change logic DIRECTORY_SEPARATOR

--- webapp/index.php
+++ webapp/index.php
@@ -1,15 +1,15 @@
 <?php
 
 // Constants
-if (empty(DIRECTORY_SEPARATOR)) {
+if (!defined('DIRECTORY_SEPARATOR')) {
 	define('DIRECTORY_SEPARATOR', (PHP_OS == 'Windows') ? '\\' : '/' );
 }
 
 // Configs
-$config = require_once '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config.php';
+$config = require '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config.php';
 
 // Get micro
-require_once $config['MicroDir'] . DIRECTORY_SEPARATOR . 'Micro.php';
+require $config['MicroDir'] . DIRECTORY_SEPARATOR . 'Micro.php';
 
 // Run application
 Micro::getInstance($config)->run();
