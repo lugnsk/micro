0c166a10e7bbe7c28851a1a2d5a32766f2ef4e11
rotiga <admin@foxtop.ru>
2014-09-19 07:49:07 +0400

    поменял стиль написания
    В 60 строке ошибка

--- micro/widgets/views/gridview.php
+++ micro/widgets/views/gridview.php
@@ -29,44 +29,45 @@ foreach ($this->rows AS $row) {
     $table[] = ['cells'=>$compileRow];
 }
 */
-?>
-<?=Html::openTag('div', $attributesCounter)?>
-    <?=$textCounter;?><?=$rowCount;?>
-<?=Html::closeTag('div')?>
-<?=Html::openTag('table', $attributes)?>
-    <?=Html::openTag('tr')?>
-        <?php foreach ($tableConfig AS $key=>$row): ?>
-            <?=Html::openTag('th')?>
-                <?= isset($row['header']) ? $row['header'] : $key ?>
-            <?=Html::closeTag('th')?>
-        <?php endforeach; ?>
-    <?=Html::closeTag('tr')?>
+echo Html::openTag('div', $attributesCounter);
+echo $textCounter;
+echo $rowCount;
+echo Html::closeTag('div');
+echo Html::openTag('table', $attributes);
+echo Html::openTag('tr');
 
-    <?php if ($filters): ?>
-        <?=Html::openTag('tr')?>
-            <?php foreach ($tableConfig AS $key=>$row): ?>
-                <?=Html::openTag('td')?>
-                    <?= isset($row['filter']) ? $row['filter'] : null ?>
-                <?=Html::closeTag('td')?>
-            <?php endforeach; ?>
-        <?=Html::closeTag('tr')?>
-    <?php endif; ?>
-
-    <?php foreach ($rows AS $elem): ?>
-        <?=Html::openTag('tr')?>
-            <?php foreach ($tableConfig AS $key=>$row): ?>
-                <?=Html::openTag('td')?>
-                    <?php if (isset($row['class']) AND is_subclass_of($row['class'], 'Micro\widgets\GridColumn')) { ?>
-                        <?= new $row['class']($row + ['str'=>isset($elem) ? $elem : null , 'key'=>$elem['id']]) ?>
-                    <?php } elseif (isset($row['value'])) { ?>
-                        <?= eval('return "'.$row['value'].'";') ?>
-                    <?php } else { ?>
-                        <?= isset($elem[$key]) ? $elem[$key] : null ?>
-                    <?php } ?>
-                <?=Html::closeTag('td')?>
-            <?php endforeach; ?>
-        <?=Html::closeTag('tr')?>
-    <?php endforeach; ?>
-
-<?=Html::closeTag('table')?>
-<?=$this->widget('Micro\widgets\PaginationWidget',$paginationConfig)?>
+foreach ($tableConfig as $key=>$row) {
+	echo Html::openTag('th');
+	echo isset($row['header']) ? $row['header'] : $key;
+	echo Html::closeTag('th');
+}
+echo Html::closeTag('tr');
+if ($filters) {
+	echo Html::openTag('tr');
+	foreach ($tableConfig as $key=>$row) 
+	{
+		echo Html::openTag('td');
+		echo isset($row['filter']) ? $row['filter'] : null;
+		echo Html::closeTag('td');
+	}
+	echo Html::closeTag('tr');
+}
+foreach ($rows as $elem) 
+{
+	echo Html::openTag('tr');
+	foreach ($tableConfig AS $key=>$row)
+	{
+		echo Html::openTag('td');
+		if (isset($row['class']) && is_subclass_of($row['class'], 'Micro\widgets\GridColumn')){ 
+			echo new $row['class']($row + ['str'=>isset($elem) ? $elem : null , 'key'=>$elem['id']]);
+		} elseif (isset($row['value'])) { 
+			eval('return "'.$row['value'].'";');
+		} else {
+			isset($elem[$key]) ? $elem[$key] : null;
+		} 
+		echo Html::closeTag('td');
+	}
+	echo Html::closeTag('tr');
+}
+echo Html::closeTag('table');
+echo $this->widget('Micro\widgets\PaginationWidget',$paginationConfig);
