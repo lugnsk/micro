eed7a0b1225ec85b95685f667877fb4f6edefca0
antivir88 <antivir88@mail.ru>
2014-09-30 10:12:47 +0700

    add Dispatcher

---
+++ micro/base/Dispatcher.php
@@ -0,0 +1,60 @@
+<?php /** MicroDispatcher */
+
+namespace Micro\base;
+
+/**
+ * Dispatcher class file.
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/antivir88/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license /LICENSE
+ * @package micro
+ * @version 1.0
+ * @since 1.0
+ */
+class Dispatcher
+{
+    /** @var array $listeners listeners objects on events */
+    protected $listeners = [];
+
+
+    /**
+     * Add listener on event
+     *
+     * @access public
+     * @param $listener
+     * @param $event
+     * @return void
+     */
+    public function addListener($listener, $event) {
+        if (!isset($this->listeners[$event])) {
+            $this->listeners[$event] = [];
+        }
+        if (!array_search($listener, $this->listeners[$event])) {
+            $this->listeners[$event][] = $listener;
+        }
+    }
+
+    /**
+     * Send signal to run event
+     *
+     * @access public
+     * @param $event
+     * @param array|null $args
+     * @return void
+     */
+    public function signal($event, $args=null) {
+        foreach ( $this->listeners AS $key => $objs ) {
+            if ($key == $event) {
+                foreach ($objs AS $obj) {
+                    if ($args) {
+                        $obj->{$event}($args);
+                    } else {
+                        $obj->{$event}();
+                    }
+                }
+            }
+        }
+    }
+}
