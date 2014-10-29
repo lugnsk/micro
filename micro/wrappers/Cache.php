261bd73d7f9f2c7c1e0f83080f14618d622f7f75
antivir88 <Poklo09o>
2014-10-29 23:55:02 +0600

    caches

--- micro/wrappers/Cache.php
+++ micro/wrappers/Cache.php
@@ -17,13 +17,25 @@ namespace Micro\wrappers;
 class Cache
 {
     protected $drivers = [
-        'array',
-        'apc',
-        'file',
-        'memcache',
-        'memcached',
-        'redis',
-        'wincache',
-        'xcache',
+        'array'     => '\\Micro\\caches\\ArrayCache',
+        'apc'       => '\\Micro\\caches\\ApcCache',
+        'file'      => '\\Micro\\caches\\FileCache',
+        'memcache'  => '\\Micro\\caches\\MemcacheCache',
+        'memcached' => '\\Micro\\caches\\MemcacheCache',
+        'redis'     => '\\Micro\\caches\\RedisCache',
+        'wincache'  => '\\Micro\\caches\\WincacheCache',
+        'xcache'    => '\\Micro\\caches\\XcacheCache',
     ];
+    protected $servers=[];
+
+    function __construct($config=[])
+    {
+        foreach ($config['servers'] AS $server) {
+            //
+        }
+    }
+    public function get($driver=null)
+    {
+        return $this->servers[$driver];
+    }
 }
