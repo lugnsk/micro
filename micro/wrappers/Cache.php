6c44328df8ce649c558335c8d7733336913a91dc
antivir88 <antivir88@mail.ru>
2014-10-30 15:41:27 +0600

    patch joined Module class into controller, patch check exsisted controller name

--- micro/wrappers/Cache.php
+++ micro/wrappers/Cache.php
@@ -1,6 +1,7 @@
 <?php /** MicroCacheWrapper */
 
 namespace Micro\wrappers;
+use Micro\base\Exception;
 
 /**
  * Cache class file.
@@ -30,12 +31,24 @@ class Cache
 
     function __construct($config=[])
     {
-        foreach ($config['servers'] AS $server) {
-            //
+        if (isset($config['servers'])) {
+            foreach ($config['servers'] AS $server) {
+                if (in_array($server['driver'], array_keys($this->drivers))) {
+                    $this->servers[] = new $this->drivers[$server['driver']] ($server);
+                } else {
+                    throw new Exception('Cache driver ' . $server['driver'] . ' not found');
+                }
+            }
+        } else {
+            throw new Exception('Caching not configured');
         }
     }
     public function get($driver=null)
     {
-        return $this->servers[$driver];
+        if (in_array($driver, $this->servers)) {
+            return $this->servers[$driver];
+        } else {
+            throw new Exception('Cache '.$driver.' not found.');
+        }
     }
 }
