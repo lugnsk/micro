50f5c5f04e3b8bc52c720b2e7b2c93bf57e2570f
antivir88 <antivir88@mail.ru>
2014-12-11 14:43:47 +0600

    Add filter interface and XSS filter

---
+++ micro/filters/CsrfFilter.php
@@ -0,0 +1,42 @@
+<?php /** CsrfFilterMicro */
+
+namespace Micro\filters;
+
+/**
+ * Class CrsfFilter
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/antivir88/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license /LICENSE
+ * @package micro
+ * @subpackage filters
+ * @version 1.0
+ * @since 1.0
+ */
+class CrsfFilter implements Filter
+{
+    /**
+     * PreFilter
+     *
+     * @access public
+     * @param array $params checked items and other params
+     * @return boolean
+     */
+    public function pre(array $params)
+    {
+        // TODO: Implement pre() method.
+    }
+
+    /**
+     * PostFilter
+     *
+     * @access public
+     * @param array $params checked items and other params
+     * @return mixed
+     */
+    public function post(array $params)
+    {
+        // TODO: Implement post() method.
+    }
+}
