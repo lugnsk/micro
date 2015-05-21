1483881aaeefd9ca54cf22469386a36020bb645c
Oleg Lunegov <zcasperx@gmail.com>
2015-05-21 12:03:50 +0600

    docblocks into abstract Action

--- micro/mvc/Action.php
+++ micro/mvc/Action.php
@@ -1,8 +1,19 @@
-<?php
+<?php /** MicroAction */
 
 namespace Micro\mvc;
 
-
+/**
+ * Class Action
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/lugnsk/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license /LICENSE
+ * @package micro
+ * @subpackage mvc
+ * @version 1.0
+ * @since 1.0
+ */
 abstract class Action
 {
     /**
