ece27e8b45d8f54f2740caace0ea98da9a1bb3e0
Zcasper <testuser@mail.linpax.org>
2015-09-29 00:51:57 +0600

    any

--- micro/file/FileHelper.php
+++ micro/file/FileHelper.php
@@ -121,7 +121,7 @@ class FileHelper
             @mkdir($dst, 0777);
         }
 
-        while (false !== ($file = readdir($dir))) {
+        while (false !== ($file = @readdir($dir))) {
             if (($file !== '.') && ($file !== '..')) {
                 if (is_dir($src . '/' . $file)) {
                     self::recurseCopyIfEdited($src . '/' . $file, $dst . '/' . $file);
@@ -130,7 +130,7 @@ class FileHelper
                         if (!file_exists($dst . '/' . $file)) {
                             @copy($src . '/' . $file, $dst . '/' . $file);
                             @chmod($dst . '/' . $file, 0666);
-                        } elseif (filemtime($src . '/' . $file) !== filemtime($dst . '/' . $file)) {
+                        } elseif (@filemtime($src . '/' . $file) !== @filemtime($dst . '/' . $file)) {
                             @copy($src . '/' . $file, $dst . '/' . $file);
                             @chmod($dst . '/' . $file, 0666);
                         }
