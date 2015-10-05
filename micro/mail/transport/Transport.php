05d33372f77b5ab08161f01d6d4143fa824d4a6a
Zcasper <testuser@mail.linpax.org>
2015-10-05 07:37:00 +0600

    PHPDoc for mail transport

--- micro/mail/transport/Transport.php
+++ micro/mail/transport/Transport.php
@@ -1,7 +1,20 @@
-<?php
+<?php /** MicroMailTransport */
 
 namespace Micro\mail\transport;
 
+/**
+ * Class Transport
+ *
+ * @author Oleg Lunegov <testuser@mail.linpax.org>
+ * @link https://github.com/lugnsk/micro
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license /LICENSE
+ * @package micro
+ * @subpackage mail\transport
+ * @version 1.0
+ * @since 1.0
+ * @abstract
+ */
 abstract class Transport implements ITransport
 {
 }
