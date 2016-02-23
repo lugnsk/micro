7ee34d4c1a1426bdaddd3278dd6f5d90ac48c360
Zcasper <testuser@mail.linpax.org>
2016-02-23 15:49:54 +0600

    simple fix namespace into console.php and move test class

--- console.php
+++ console.php
@@ -4,6 +4,6 @@ require __DIR__ . '/micro/Micro.php';
 
 $app = new \Micro\Micro(__DIR__ . '/app', __DIR__ . '/micro');
 
-$app->run(new \Micro\web\Request)->send();
+$app->run(new \Micro\Web\Request)->send();
 
 $app->terminate();
