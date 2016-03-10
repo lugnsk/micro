d6bb8e05fc70d283f703d936d9f95ccbfeabe349
Zcasper <testuser@mail.linpax.org>
2016-03-11 01:29:25 +0600

    Composer

--- app/__autoload.php
+++ app/__autoload.php
@@ -1,9 +1,9 @@
 <?php
 
-require __DIR__ . '/../micro/base/Autoload.php';
+require __DIR__ . '/../vendor/linpax/microphp-framework/base/Autoload.php';
 
 spl_autoload_register(['\Micro\Base\Autoload', 'loader'], true, false);
 
-\Micro\Base\Autoload::setAlias('Micro', __DIR__ . '/../micro');
+\Micro\Base\Autoload::setAlias('Micro', __DIR__ . '/../vendor/linpax/microphp-framework');
 \Micro\Base\Autoload::setAlias('App', __DIR__);
 \Micro\Base\Autoload::setAlias('Web', getenv('DOCUMENT_ROOT'));
